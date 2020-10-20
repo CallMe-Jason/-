2bcaaab7b65abbe2653888c0e73d5fa431d791f5
刘皓东 <2872213719@qq.com>
2020-10-20 15:09:22 +0800

    记录

---
+++ JS基础/JS基础01/js/外链式.js
@@ -0,0 +1,9 @@
+console.log ('呀啊哈');
+console.log ('呀哈');
+console.log ('呀');
+prompt("输入您的年龄");
+document.write ('<a href="#">啦哈哈</a>');
+var age;
+age = 11;
+document.write ('您的年龄是');
+document.write (age);
